1cd0b36b3738309543fc70a8c60386d5aa80bba9
michalgebauer <michal.gebauer@gmail.com>
2017-06-16 16:51:50 +0200

    Separated proxy-login.js

--- public/proxy.js
+++ public/proxy.js
@@ -1,44 +1,4 @@
 $(document).ready(function() {
-
-    $("#login").click(function(e) {
-        e.preventDefault();
-
-        var loginData = {
-            username: $("#username").val(),
-            password: $("#password").val()
-        };
-
-        $.ajax({
-            url: "proxy/rest/auth/1/session",
-            type: "post",
-            contentType: "application/json",
-            headers: {
-                "X-Atlassian-Token": "no-check"
-            },
-            data: JSON.stringify(loginData)
-        }).done(function() {
-            $.ajax({
-                url: "proxy/rest/api/2/myself",
-                type: "get",
-                dataType: "json"
-            }).done(function(me) {
-                $("#hello").text(me.key);
-            });
-
-            $.ajax({
-                url: "proxy/rest/api/2/issue/P1419-557",
-                type: "get",
-                dataType: "json"
-            }).done(function(issue) {
-                console.log(issue);
-            });
-
-            // project = P1419 AND issuetype = Activity AND status = Closed AND assignee in (currentUser())
-
-            
-        })
-    })
-
     $("#searchButton").click(function(e) {
         e.preventDefault();
 
