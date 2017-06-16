8f4621b3bcc7a22da1d9b94cce1456c7b85b398c
michalgebauer <michal.gebauer@gmail.com>
2017-06-16 15:53:23 +0200

    Fixed: login call before every rest ajax not needed

--- public/proxy.js
+++ public/proxy.js
@@ -42,52 +42,41 @@ $(document).ready(function() {
     $("#searchButton").click(function(e) {
         e.preventDefault();
 
-        var $login = $.ajax({
-            url: "proxy/rest/auth/1/session",
+        $.ajax({
+            url: "proxy/rest/api/2/search",
             type: "post",
             contentType: "application/json",
             headers: {
                 "X-Atlassian-Token": "no-check"
             },
             data: JSON.stringify({
-                username: $("#username").val(),
-                password: $("#password").val()
-            })
-        });
+                "jql": $("#jql").val(),
+                "startAt": 0,
+                "maxResults": 15,
+                "fields": [
+                    "summary",
+                    "status",
+                    "assignee"
+                ]
+            }),
+            dataType: "json"
+        }).done(function(result) {
+            console.log(result);
+            
+            // vymaz obsah tabulky:
+            var $table = $("#search tbody");
+            $("tr", $table).remove();
 
-        $.when($login).then(function() {
-            $.ajax({
-                url: "proxy/rest/api/2/search",
-                type: "post",
-                contentType: "application/json",
-                headers: {
-                    "X-Atlassian-Token": "no-check"
-                },
-                data: JSON.stringify({
-                    "jql": $("#jql").val(),
-                    "startAt": 0,
-                    "maxResults": 15,
-                    "fields": [
-                        "summary",
-                        "status",
-                        "assignee"
-                    ]
-                }),
-                dataType: "json"
-            }).done(function(result) {
-                console.log(result);
+            result.issues.forEach(function(issue) {
 
-                result.issues.forEach(function(issue) {
+                var $tr = $("<tr>");
 
-                   var $tr = $("<tr>");
+                $tr.append("<td><a href='/browse/" + issue.key +"'>" + issue.key + "</a></td>");
+                $tr.append("<td>" + issue.fields.assignee.displayName + "</td>");
+                $tr.append("<td>" + issue.fields.summary + "</td>");
 
-                   $tr.append("<td><a href='/browse/" + issue.key +"'>" + issue.key + "</a><td>");
-                   $tr.append("<td>" + issue.fields.assignee.displayName + "</td>");
-                   $tr.append("<td>" + issue.fields.summary + "</td>");
-
-                   $("#search tbody").append($tr);
-                });
-            }); 
-        })
+                $table.append($tr);
+            });
+        }); 
     })
 })
--- server.js
+++ server.js
@@ -6,7 +6,11 @@ var cookieParser = require('cookie-parser');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
 var app = express();
-var sesssionCookies = "";
+
+var sesssionCookies = {
+    jsessionid: "",
+    xsrfToken: ""
+}
 
 app.use(express.static('public'));
 app.use(cookieParser());
@@ -14,26 +18,29 @@ app.use('/proxy', proxy('https://jiratest.uniqa.at', {
     proxyReqOptDecorator: function(proxyReqOpts, srcReq) {
         // set origin to jira url to bypass cross domain problem
         proxyReqOpts.headers['Origin'] = 'https://jiratest.uniqa.at';
-        if(sesssionCookies) {
-            proxyReqOpts.headers['cookie'] = sesssionCookies;
+
+        var cookies = [];
+        for(cookie in sesssionCookies) {
+            cookies.push(sesssionCookies[cookie]);
         }
+        if(cookies.length) {
+            proxyReqOpts.headers['cookie'] = cookies.join(";");
+        }
+
         // proxyReqOpts['path'] = '/rest' + proxyReqOpts.path;
         // proxyReqOpts.params['path'] = proxyReqOpts.path;
         return proxyReqOpts;
     },
     userResDecorator: function(proxyRes, proxyResData, userReq, userRes) {
-        var newSessionCookies = [];
-
         if(proxyRes.headers['set-cookie']) {
             proxyRes.headers['set-cookie'].forEach(function(cookie) {
-                if(cookie.indexOf("JSESSIONID=") != -1 || cookie.indexOf("atlassian.xsrf.token=") != -1) {
-                    newSessionCookies.push(cookie.split(";")[0]);
+                if(cookie.indexOf("JSESSIONID=") != -1) {
+                    sesssionCookies.jsessionid = cookie.split(";")[0];
+                }
+                if(cookie.indexOf("atlassian.xsrf.token=") != -1) {
+                    sesssionCookies.xsrfToken = cookie.split(";")[0];
                 }
             });
-        }
-
-        if(newSessionCookies.length) {
-            sesssionCookies = newSessionCookies.join(";");
         }
 
         // function must return data
